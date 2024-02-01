94c6b8371a73727481da710967efa05ece7ac309
Dangog <cajedanilo@gmail.com>
2024-02-01 20:59:28 -0300

    First Commit

---
+++ src/main/java/br/com/fipeConsume/br/com/alura/estudos/danilo/fipeConsume/Application.java
@@ -0,0 +1,13 @@
+package br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fipeConsume/br/com/alura/estudos/danilo/fipeConsume/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
