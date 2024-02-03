c1fc889b4af66218ef84542866729b260d4f2bfb
Dangog <cajedanilo@gmail.com>
2024-02-03 12:35:00 -0300

    Added car model search

--- src/main/java/br/com/fipeConsume/br/com/alura/estudos/danilo/fipeConsume/principal/Principal.java
+++ src/main/java/br/com/fipeConsume/br/com/alura/estudos/danilo/fipeConsume/principal/Principal.java
@@ -1,12 +1,15 @@
 package br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume.principal;
 
+import br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume.model.Models;
 import br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume.model.VehicleBrandsData;
 import br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume.service.APIConsume;
+import br.com.fipeConsume.br.com.alura.estudos.danilo.fipeConsume.service.JacksonDataConverter;
 import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import com.fasterxml.jackson.databind.util.ISO8601Utils;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,7 +19,7 @@ public class Principal {
     public void showMenu(){
 
         APIConsume apiConsume = new APIConsume();
-
+        JacksonDataConverter conv = new JacksonDataConverter();
         Scanner t = new Scanner(System.in);
 
         System.out.println("**** OPÇÕES **** ");
@@ -37,22 +40,29 @@ public class Principal {
                 break;
         }
 
-       String vehicleList = apiConsume.getData("https://parallelum.com.br/fipe/api/v1/" + vehicleType + "/marcas");
+        String completeURL = "https://parallelum.com.br/fipe/api/v1/" + vehicleType + "/marcas";
 
+       String brandList = apiConsume.getData(completeURL);
 
+        var vehicleBrandsData = conv.getList(brandList, VehicleBrandsData.class);
 
+        vehicleBrandsData.stream()
+                        .sorted(Comparator.comparing(VehicleBrandsData::code))
+                                .forEach(System.out::println);
 
-        List<VehicleBrandsData> vehicleBrandsData = new ArrayList<>();
+        System.out.println("Informe o código da marca para consulta: ");
+        String brandCodeResp = t.nextLine();
 
+        String vehiclePerBrandList = apiConsume.getData(completeURL + "/" + brandCodeResp + "/modelos");
+        var modelsList = conv.getData(vehiclePerBrandList, Models.class);
 
+        System.out.println("Modelos da marca especificada: ");
+        modelsList.models().stream()
+                .sorted(Comparator.comparing(VehicleBrandsData::code))
+                .forEach(System.out::println);
 
+        System.out.println("Digite um trecho do nome do carro para consulta: ");
+        String carForSearch = t.nextLine();
+        
     }
-
-
-
-    APIConsume apiConsume = new APIConsume();
-
-
-
-
 }
